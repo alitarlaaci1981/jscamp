067e8a239c231a75b1f5e30d405f54abcb075f30
ali <kemal_tar@hotmail.com>
2022-10-03 09:21:11 -0500

    october 2 practice finished

--- src/app.js
+++ src/app.js
@@ -36,3 +36,29 @@
 // console.log(konutKredileri)
 
 console.log("Merhaba kodlama");
+//javascript type safe degildir.
+let dolarBugun = 9.30
+
+let dolarDun=9.20
+
+
+{
+let dolarDun=9.10
+}
+console.log(dolarDun);
+
+const euroDun=11.2
+//euroDun=11 we cant reassign const value
+
+console.log(euroDun);
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut kredisi","Emlak konut kredisi","Kamu konut kredis","Ozel konut kredileri"];
+
+for (let i = 0; i<konutKredileri.length; i++) {
+     console.log("<li>" + konutKredileri[i] + "</li>");
+    }
+
+console.log(konutKredileri);
+
